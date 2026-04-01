cf298c5561e196988fbfd05e1e604f2fe243d99f
Isaac Inacio <isaakinf@gmail.com>
2026-04-01 20:20:11 -0300

    Refactor: Ajuste na pagina de adm, estilização do filtro da tabela

--- gestao_adocao/urls.py
+++ gestao_adocao/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     path('forms/', views.forms, name="forms"),
     path('admin/', views.lista_cadastros, name="admin"),
     path('admin/user/<int:id>/', views.user, name="user"),
-<<<<<<< HEAD
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name="logout"),
-=======
->>>>>>> 4f0752425844aaa688ff54e09df4d8323aa38cf4
 ]
